8523b65b399bd5427f5d72e9888b4c76e4ce2cb4
Davi Dev <davideveloper0@gmail.com>
2026-09-11 14:46:00 -0300

    Beecrowd 1021

--- Learning/Beecrowd/Exercicio-1021/main.c
+++ Learning/Beecrowd/Exercicio-1021/main.c
@@ -1,19 +1,92 @@
 #include <stdio.h>
 
-int main()
-{
-    int Entrada, Resto,
-        Horas, Minutos, Segundos;
+int main() {
+    float ValorMonetario;
+    int Notas[6], Moedas[6], Centavos, MonetarioInt, Resto;  // Notas e moedas decrescentes, 100, 50, 20...
 
-    scanf("%d", &Entrada);
+    // Resto é usada como uma variável auxiliar, guardando o resto de todas as divisões
 
-    Horas = Entrada / 3600;
-    Resto = Entrada % 3600;
+    // Lendo Valor Monetario
+    scanf("%f", &ValorMonetario);
 
-    printf("Resto: %d\n", Resto);
+    // Caso o valor seja alto demais ou negativo
+    if (ValorMonetario <= 0 || ValorMonetario >= 1000000.0) return 0;
 
-    Minutos = Resto / 60;
-    Segundos = Resto % 60;
+    // Transformando valor monetario em inteiro
+    MonetarioInt = (int)ValorMonetario;
 
-    printf("%d:%d:%d\n", Horas, Minutos, Segundos);
+    // Quantidade de Notas
+
+    Notas[0] = MonetarioInt / 100;
+    Resto = MonetarioInt % 100;
+
+    Notas[1] = Resto / 50;
+    Resto = Resto % 50;
+
+    Notas[2] = Resto / 20;
+    Resto = Resto % 20;
+
+    Notas[3] = Resto / 10;
+    Resto = Resto % 10;
+
+    Notas[4] = Resto / 5;
+    Resto = Resto % 5;
+
+    Notas[5] = Resto / 2;
+    Resto = Resto % 2;
+
+    // Quantidade de Moedas
+
+    Centavos = Resto * 100;
+
+    Moedas[0] = Centavos / 100;
+
+    Resto = (Centavos % 100);
+
+    Moedas[1] = Resto / 50;
+    Resto = Resto % 50;
+
+    Moedas[2] = Resto / 25;
+    Resto = Resto % 25;
+
+    Moedas[3] = Resto / 10;
+    Resto = Resto % 10;
+
+    Moedas[4] = Resto / 5;
+    Resto = Resto % 5;
+
+    Moedas[5] = Resto;
+
+    // Printando
+
+    printf(
+        "NOTAS:\n"
+        "%d nota(s) de R$ 100.00\n"
+        "%d nota(s) de R$ 50.00\n"
+        "%d nota(s) de R$ 20.00\n"
+        "%d nota(s) de R$ 10.00\n"
+        "%d nota(s) de R$ 5.00\n"
+        "%d nota(s) de R$ 2.00\n"
+        "MOEDAS:\n"
+        "%d moeda(s) de R$ 1.00\n"
+        "%d moeda(s) de R$ 0.50\n"
+        "%d moeda(s) de R$ 0.25\n"
+        "%d moeda(s) de R$ 0.10\n"
+        "%d moeda(s) de R$ 0.05\n"
+        "%d moeda(s) de R$ 0.01\n",
+        Notas[0],
+        Notas[1],
+        Notas[2],
+        Notas[3],
+        Notas[4],
+        Notas[5],
+        Moedas[0],
+        Moedas[1],
+        Moedas[2],
+        Moedas[3],
+        Moedas[4],
+        Moedas[5]
+    );
+
+    return 0;
 };
